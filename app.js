44b60f8da2bd9806a5b8f143b9abf3bf817dd50a
yamama95 <yamamayahia954@gmail.com>
2020-02-22 19:45:23 +0200

    p5

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 function sumArray(sumArr) { //eslint-disable-line
 var number=0;
 for (var j=0;j<testArray.length;j=j+1){
-    number= sum(number+testArray[j])[0];
+    number= sum(number,testArray[j])[0];
 var message='2,3,4 was passed in as an array of numbers, and 9 is their sum'
 var result=[number,message];
 return result;
@@ -108,11 +108,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    var number=0;
+    for (var j=0;j<testArray.length;j=j+1){
+        number= multiply(number,testArray[j])[0];
+    var message='The numbers 2,3,4 have a product of 24.'
+    var result=[number,message];
+    return result;
+    }
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -126,7 +132,7 @@ Write a function called multiplyAnyArray() that takes an array of numbers of any
 
 'The numbers 1,2,3,4,5 have a product of 120.'
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use tsshe arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 
@@ -142,4 +148,4 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the canvas'
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the canvas);
